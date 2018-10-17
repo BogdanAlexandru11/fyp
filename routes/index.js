033622171f093b88ae1345d386d18418a7d6df2b
alex <abcbogdan11@gmail.com>
2018-10-17 21:05:19 +0100

    removed input fields from the home page

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,6 @@ var connection = mysql.createConnection({
     database : 'fyp'
 });
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
     connection.query('SELECT * FROM car_data', function (error, results, fields) {
@@ -25,21 +24,25 @@ router.post('/', function(req, res, next) {
     console.log("RESRESRESRESRESRESRESRESRESRESRESRES");
     console.log(res);
     console.log("RESRESRESRESRESRESRESRESRESRESRESRES");
-});
-
-router.get('/insert_into_db', function(req, res, next) {
-    console.log(req.query.insertCarRegIntoDB);
-    connection.query('INSERT INTO car_data (car_reg) VALUES (?)', [req.query.insertCarRegIntoDB], function (err, result) {
-    res.redirect('/');
+    connection.query('SELECT * FROM car_data', function (error, results, fields) {
+        //check here if the car is in the database, if it's not, send an email stating that it is not in the database
+        res.render('index', { car_data : results });
     });
 });
 
-router.get('/check_value_in_db', function(req, res, next) {
-    connection.query('SELECT * FROM car_data WHERE car_reg=?', [req.query.checkValueInDB], function (err, result) {
-        console.log(result);
-        res.redirect('/');
-    });
-});
+// router.get('/insert_into_db', function(req, res, next) {
+//     console.log(req.query.insertCarRegIntoDB);
+//     connection.query('INSERT INTO car_data (car_reg) VALUES (?)', [req.query.insertCarRegIntoDB], function (err, result) {
+//     res.redirect('/');
+//     });
+// });
+//
+// router.get('/check_value_in_db', function(req, res, next) {
+//     connection.query('SELECT * FROM car_data WHERE car_reg=?', [req.query.checkValueInDB], function (err, result) {
+//         console.log(result);
+//         res.redirect('/');
+//     });
+// });
 
 
 module.exports = router;
