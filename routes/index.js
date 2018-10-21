d00fa845e07c47775307507faa673fd82bd783af
alex <abcbogdan11@gmail.com>
2018-10-21 16:08:08 +0100

    update

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ var moment = require('moment');
 var Promise = require('promise');
 var nodemailer = require('nodemailer');
 let {PythonShell} = require('python-shell');
+var log = require('fancy-log');
+const debug = require('debug')('my-namespace')
+const name = 'my-app'
+debug('booting %s', name)
 
 
 var connection = mysql.createConnection({
@@ -23,6 +27,7 @@ var transporter = nodemailer.createTransport({
 
 
 router.get('/', function (req, res, next) {
+    log.info();
     connection.query('SELECT * FROM car_data ORDER BY id DESC', function (error, results, fields) {
 
         // new Promise(function(resolve, reject) {
@@ -53,11 +58,15 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', function (req, res, next) {
+    log("i got in the post req");
+
 
     var regex = /\d{2,3}[a-zA-Z]{1,2}\d{1,6}/;
 
     //change this to the sender lol
     if (true) {
+        log("true statement");
+
         res.end();
         var car_data = {
             // car_reg: req.body.best_plate.plate,
@@ -71,27 +80,40 @@ router.post('/', function (req, res, next) {
 
 
         function resolveAfter2Seconds() {
+            log("resolve after 2 sec promise");
+
             return new Promise(resolve => {
+                log("resolve after 2 sec promise - inside promise");
+
 
                 connection.query('SELECT * FROM valid_permits', function (error, results, fields) {
+                    log("cquery select * from val perm");
                     for (var i = 0; i < results.length; i++) {
                         results[i].car_reg = results[i].car_reg.replace('-', '').toUpperCase();
                         car_data.car_reg = car_data.car_reg.toUpperCase();
                         if (results[i].car_reg === car_data.car_reg) {
                             car_data.valid_permit = 'true';
+                            log("found car with valid permit");
                         }
                     }
                 });
                 if (car_data.car_reg.match(regex)) {
+                    log("car data matches regex");
+
                     //TODO fix this, timing + too many connections, maybe create a pool?
                     console.log("matches regex");
                     console.log("i got in promise");
                     let pyshell = new PythonShell('../fyp/checkNct.py', {pythonPath: '/usr/bin/python'});
                     pyshell.send(car_data.car_reg);
+                    log("just sent the plate to the script");
+
 
                     pyshell.on('message', function (message) {
+
                         // console.log("message " + message);
                         car_data.nct = message;
+                        log("got data from the script " + message);
+
                         resolve(message);
                     });
                 }
@@ -100,14 +122,19 @@ router.post('/', function (req, res, next) {
 
 
         async function asyncCall() {
-            console.log('calling');
+            log("async call func");
+
             var result = await resolveAfter2Seconds();
             console.log(result);
             connection.query('INSERT INTO car_data (car_reg, date, valid_permit, x_coord, y_coord, nct) VALUES (?,?,?,?,?,?)', [car_data.car_reg, car_data.date, car_data.valid_permit, car_data.x_coord, car_data.y_coord, car_data.nct], function (err, result) {
+                log("inserted data");
+
                 console.log(car_data);
-                console.log("inserted into the db");
+                log("inserted into the db");
             });
             if (car_data.valid_permit === 'false') {
+                log("email sent");
+
                 var mailOptions = {
                     from: 'alex.fyp2018@gmail.com', // sender address
                     to: 'abcbogdan11@gmail.com', // list of receivers
