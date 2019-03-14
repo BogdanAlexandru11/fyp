7a23c77c28ad82397a3b4d0937cf000d52b0ad13
alex_laptop <abcbogdan11@gmail.com>
2019-03-14 11:49:02 +0000

    added comments

--- routes/index.js
+++ routes/index.js
@@ -21,13 +21,15 @@ var ExifImage = require('exif').ExifImage;
 
 
 
-
+//connection the the mysql
+//credentials are being stored in a .env file
 var connection = mysql.createConnection({
     host: process.env.dbhost,
     user: process.env.dbuser,
     password: process.env.dbpw,
     database: process.env.dbdb
 });
+//npm package used to send the email
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -36,14 +38,17 @@ var transporter = nodemailer.createTransport({
     }
 });
 
-
+// the '/' route represents the "http://46.101.52.245:3000", and it uses cookies to keep the user logged in
 router.get('/', function (req, res, next) {
+    //if the uses is logged in i.e if the user inserted the right password, redirect them to the index page
     if(req.session.user){
+        //prints only the cars which have an id greater than 340, for the homepage not to be too long
         connection.query('SELECT * FROM car_data WHERE id > 340 ORDER BY id DESC', function (error, results, fields) {
             log("GET /");
             res.render('index', {car_data: results});
         });
     }
+        //otherwise the user is not logged in, therefore they will be redirected to the homepage of the application
     else{
         res.render('homepage');
     }
@@ -54,10 +59,12 @@ router.post('/', function (req, res, next) {
     log("POST /");
 });
 
+//handles the login on the application
 router.post('/login', function (req, res, next) {
     if(req.body.inputText===process.env.websitepw){
         log("USER LOGGED IN");
         req.session.user='true';
+        //the cookie expires in 30 minutes
         req.session.cookie.expires = new Date(Date.now() + hour);
         req.session.cookie.maxAge = hour;
         res.redirect('/');
@@ -66,27 +73,26 @@ router.post('/login', function (req, res, next) {
         res.redirect('/');
     }
 });
-
+//user logs out therefore the cookie will became null
 router.post('/logout', function (req, res, next) {
     log("USER LOGGED OUT");
     req.session.user = null;
     res.redirect('/');
 });
 
-
+//the post request received from OpenALPR
 router.post('/alprPOST', function (req, res, next) {
+    //using res.end(), so the response ends
     res.end();
     if((req._parsedUrl.query==="postedFromOpenAlprfyp2018")){
-        // console.log(req.body.vehicle.color);
-        console.log(req.body.vehicle.color[0].name);
-        console.log(req.body.vehicle.body_type[0].name);
-        console.log(req.body.vehicle.make_model[0].name);
-
-        // var regex = /\d{2,3}[(CW)]\d{1,6}/;
+        //the endpoint is /postedFromOpenAlprfyp2018 therefore it will enter in this if statemtn
+        //the reg plate is being stripped to be verify whether it is an irish reg plate
         var regex =/\d{1,3}(KK|kk|ww|WW|c|C|ce|CE|cn|CN|cw|CW|d|D|dl|DL|g|G|ke|KE|ky|KY|l|L|ld|LD|lh|LH|lk|LK|lm|LM|ls|LS|mh|MH|mn|MN|mo|MO|oy|OY|so|SO|rn|RN|tn|TN|ts|TS|w|W|wd|WD|wh|WH|wx|WX)\d{1,6}/;
-        //change this to the sender lol
+
         if (req.body.agent_type==='alprd' || req.body.debug==='fyp_true') {
+            //the following code block is being used for local development, i.e the drone doesnt have to be on in order to work on the code
             if (localEnv==='true'){
+                //the post requests sent by POSTMAN
                 log("POST REQUEST ON LOCALHOST FROM POSTMAN");
                 var car_data = {
                     id :'',
@@ -98,6 +104,7 @@ router.post('/alprPOST', function (req, res, next) {
                 };
             }
             else{
+                //post requests done by POSTMAN but on the server
                 log("local env false");
                 if(req.body.debug==='fyp_true'){
                     log("");
@@ -111,6 +118,7 @@ router.post('/alprPOST', function (req, res, next) {
                     };
                 }
                 else{
+                    //the live data being received from the drone
                     log("LIVE FROM DRONE");
                     var car_data = {
                         id :'',
@@ -128,8 +136,11 @@ router.post('/alprPOST', function (req, res, next) {
             log("reg plate "+ car_data.car_reg);
             log("****************");
 
+            //the following functions handle the python script workflow
+            //the following JS blocks were converted into syncronous code
             function firstFunction() {
                 return new Promise(resolve => {
+                    //the reg plate received is checked to verify whether it is in the valid permits database
                     connection.query('SELECT * FROM valid_permits', function (error, results, fields) {
                         for (var i = 0; i < results.length; i++) {
                             results[i].car_reg = results[i].car_reg.replace('-', '').toUpperCase();
@@ -142,7 +153,7 @@ router.post('/alprPOST', function (req, res, next) {
                     });
                     if (car_data.car_reg.toUpperCase().match(regex)) {
                         var thisRequestTime=new Date();
-
+                        //the script is being called on the local environment
                         if(localEnv==='true'){
                             //localhost testing
                             let pyshell = new PythonShell('../fyp/checkNct.py', {pythonPath: '/usr/bin/python'});
@@ -169,8 +180,8 @@ router.post('/alprPOST', function (req, res, next) {
 
                         }
                         else{
+                            //the script is being called on the server i.e used by the drone
                             let pyshell = new PythonShell('/opt/live/my-first-app/checkNct.py', {pythonPath: '/usr/bin/python'});
-                            //the script that is being called on the droplet
                             if(thisRequestTime.getTime() - lastRequestTime.getTime() > 20000){
                                 timeDelay=1000;
                             }
@@ -187,6 +198,7 @@ router.post('/alprPOST', function (req, res, next) {
                             }
                             log("plate sent to the script");
                             pyshell.on('message', function (message) {
+                                //the resulting NCT expiry date will be stored in the "message" variable
                                 car_data.nct = message;
                                 resolve(message);
                             });
@@ -199,8 +211,10 @@ router.post('/alprPOST', function (req, res, next) {
                 });
             }
             async function thirdFunction(){
+                //this function waits for the "firstfunction" to finish and send the data before it is being ran
                 var result = await firstFunction();
                 connection.query('INSERT INTO car_data (car_reg, date, valid_permit, gps_coord, nct, car_make, car_body, car_colour) VALUES (?,?,?,?,?,?,?,?)', [car_data.car_reg, car_data.date, car_data.valid_permit, car_data.gps_coord, car_data.nct, req.body.vehicle.make_model[0].name, req.body.vehicle.body_type[0].name, req.body.vehicle.color[0].name], function (err, result) {
+                    //data being inserted into the database
                     if (err)
                         console.log(err);
                     console.log(car_data);
@@ -210,11 +224,12 @@ router.post('/alprPOST', function (req, res, next) {
 
             async function secondFunction() {
                 var result = await thirdFunction();
-
+                //function that handles the emails being sent and the GPS coordinates
                 connection.query('SELECT * FROM car_data where date=?', [car_data.date], function (errorrr, results, fields) {
                     console.log(results[0]);
                     //the id will be results[0].id;
                     console.log("car id is " +results[0].id);
+                    //as the gps coordinates functionality wasn't finished, I hardcoded them
                     const carId=results[0].id;
                     const latitude="53.34888941592108";
                     const longitude="-7.0455235477066065";
@@ -222,13 +237,8 @@ router.post('/alprPOST', function (req, res, next) {
                     const gpsCOORDs=latitude.substr(0,10) + " " + longitude.substr(0,10);
 
 
-                    // connection.query('UPDATE car_data set gps_coord=?, altitude=?, where id=?', [gpsCOORDs,altitude,carId], function (errorrr, results, fields) {
-                    //     console.log("errror here " + errorrr);
-                    //     console.log("result" + result);
-                    //     log("gps coord were updated");
-                    // });
-
                     connection.query('update car_data set gps_coord = ?, altitude =? where id=?;',[gpsCOORDs,altitude,carId], function (errorrr, results, fields) {
+                        //the data is being updated into the database based on the id of the plate recognition
                         console.log("errror here " + errorrr);
                         log("gps coord were updated");
                     });
@@ -255,7 +265,9 @@ router.post('/alprPOST', function (req, res, next) {
 
                 }); 
 
-                //used to store the images!!!
+                //the images are being downloaded from the openalpr agent
+                //and they're being stored in the /my-first-app/public/images./
+                //they're being stored having the name the id of the recognition
 
                 connection.query('SELECT * FROM car_data where car_reg=? ORDER BY id DESC', [car_data.car_reg], function (errorrr, results, fields) {
                     if(errorrr)
@@ -276,10 +288,12 @@ router.post('/alprPOST', function (req, res, next) {
                         },
                     );
                 });
-                //
+                //if the car doesnt have a valid parking permit
+                //an email is being sent to the admin
                 if (car_data.valid_permit === 'false') {
                     log("email sent");
                     var mailOptions = {
+                        //TODO add more information to the email, even the picture
                         from: 'alex.fyp2018@gmail.com', // sender address
                         to: 'abcbogdan11@gmail.com', // list of receivers
                         subject: 'Car park updates', // Subject line
@@ -301,13 +315,7 @@ router.post('/alprPOST', function (req, res, next) {
     res.end();
 });
 
-
-router.post('/ALPRDAEMONTEST', function (req, res, next) {
-    res.end();
-    log("ALP /");
-});
-
-
+//the GPS data received from the Android Application created
 router.post('/GPSDATA', function (req, res, next) {
     res.end();
     log(req.body);
@@ -384,7 +392,7 @@ router.post('/GPSDATA', function (req, res, next) {
 // }
 
 
-
+//function to replace the character at a given index in a string
 String.prototype.replaceAt=function(index, replacement) {
     return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
 };
@@ -416,6 +424,7 @@ String.prototype.replaceAt=function(index, replacement) {
 //     res.end();
 // });
 
+//handles the return to home on the 404 page
 router.post('/home', function (req, res, next) {
     if(req.session.user === null){
         res.render('/homepage');
